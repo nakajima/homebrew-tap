0636af3f907ed85103aec8ac3b64e0d9841871db
Pat Nakajima <patnakajima@gmail.com>
2024-05-15 15:15:30 -0700

    ohno 0.0.11
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ohno.rb
+++ Formula/ohno.rb
@@ -1,8 +1,8 @@
 class Ohno < Formula
   desc "Static blog generator, opinionated, probably works?"
   homepage "https://github.com/nakajima/ohno"
-  url "https://github.com/nakajima/ohno/releases/download/0.0.10/ohno.zip"
-  sha256 "63023b473bcbfa519e77c516fdbfe5db88ae6676191b728314c8f6c11390d6e7"
+  url "https://github.com/nakajima/ohno/releases/download/0.0.11/ohno.zip"
+  sha256 "4a0790f00cc139b1c76ee98a93fb3f37a0cf8f8fbd18eb429a12a7fb624691a2"
   license "BSD-3-Clause"
 
   def install
