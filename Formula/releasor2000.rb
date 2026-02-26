fe67f642ea9ba9fccde240b17bb5dcef7d430f8a
Pat Nakajima <patnakajima@gmail.com>
2026-02-26 15:47:03 -0800

    Update releasor2000 to 0.0.9

--- Formula/releasor2000.rb
+++ Formula/releasor2000.rb
@@ -1,16 +1,16 @@
 class Releasor2000 < Formula
   desc "releasor2000"
   homepage "https://github.com/nakajima/releasor2000"
-  version "0.0.8"
+  version "0.0.9"
 
   on_macos do
     on_arm do
-      url "https://github.com/nakajima/releasor2000/releases/download/v0.0.8/releasor2000-0.0.8-aarch64-apple-darwin.tar.gz"
-      sha256 "0f5df4b19f25060d560607b768bad0eaa39f3c175fa438dbf2e5f5d0e49b3f1a"
+      url "https://github.com/nakajima/releasor2000/releases/download/v0.0.9/releasor2000-0.0.9-aarch64-apple-darwin.tar.gz"
+      sha256 "222746cf6eca8ba29a752d7e80e571785cad2d2d89ef192fc7a4ae8589e8b0ab"
     end
     on_intel do
-      url "https://github.com/nakajima/releasor2000/releases/download/v0.0.8/releasor2000-0.0.8-x86_64-apple-darwin.tar.gz"
-      sha256 "8533b7d027c66626e41880104f23a6f477b7779ae171ff3b25b66775a79b498a"
+      url "https://github.com/nakajima/releasor2000/releases/download/v0.0.9/releasor2000-0.0.9-x86_64-apple-darwin.tar.gz"
+      sha256 "6fd9c29913f580f58841e4e5be4a9b507e948b3fb172c44a5743db9e0ee2057d"
     end
   end
 
